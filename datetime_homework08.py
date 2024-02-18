f84a908bae41aaa6c0485ab9bf32deb58b7de744
Abduvaliyevakumushxon <shodmonovd1985@gmail.com>
2024-02-18 20:11:50 +0500

    8-vazifa tugadi

--- datetime_homework08.py
+++ datetime_homework08.py
@@ -1,2 +1,5 @@
 #Task 8:
+import datetime
+data = datetime.datetime.today()
+print(data.hour-10, data.minute)
 #Write a Python program that subtracts 10 hours from the current time and displays the resulting time.
